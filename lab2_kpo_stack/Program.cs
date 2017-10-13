50352817231f305c46c833ce07e1abecb2750635
andymiskovsky <nuclearhiems@hotmail.com>
2017-10-14 00:48:11 +0300

    Added class main

--- lab2_kpo_stack/Program.cs
+++ lab2_kpo_stack/Program.cs
@@ -10,6 +10,27 @@ namespace lab2_kpo_stack
     {
         static void Main(string[] args)
         {
+            NodeStack<string> stack = new NodeStack<string>();
+            stack.Push("Tom");
+            stack.Push("Alice");
+            stack.Push("Bob");
+            stack.Push("Kate");
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("_____");
+            string header = stack.Peek();
+            Console.WriteLine($"Stack head: {header}");
+            Console.WriteLine("_____");
+            header = stack.Pop();
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("______________________________________");
+            Console.ReadKey();
         }
     }
 }
